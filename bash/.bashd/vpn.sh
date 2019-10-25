85b58e39f9bee053e93b1a1aa968d561e053f21c
Michael Meffie <mmeffie@sinenomine.net>
2019-10-25 14:17:11 -0400

    add vpn-status alias

--- bash/.bashd/vpn.sh
+++ bash/.bashd/vpn.sh
@@ -1,2 +1,4 @@
-alias vpn-start='sudo systemctl start openvpn-client@mmeffie'
-alias vpn-stop='sudo systemctl start openvpn-client@mmeffie'
+SERVICE="openvpn-client@mmeffie"
+alias vpn-start="sudo systemctl start $SERVICE"
+alias vpn-status="sudo systemctl status $SERVICE"
+alias vpn-stop="sudo systemctl stop $SERVICE"
